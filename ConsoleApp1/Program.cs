5f1fcefae587a42084b08d7c742d1a3f5be30ff5
Lenovo <Lenovo@DESKTOP-NV5PSHA>
2023-10-06 21:50:11 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+
+namespace adadd_aval
+{
+    class adadd_aval
+    {
+        static void Main()
+        {
+            // Ali vahedi
+            // Adadd_Aval progect
+            Console.WriteLine("please insert your first number : ");
+            int aval_Baze = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please insert your second number : ");
+            int akhar_Baze = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            for (int i = aval_Baze; i < akhar_Baze; i++)
+            {
+                if (mohasebeh(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            static  bool mohasebeh(int number)
+            {
+                for (int j = 2; j < number; j++)
+                {
+                    if (number % j == 0 && number != j)
+                    {
+                        return false;
+                    }
+                   
+                }
+                return true;
+            }
+            Console.ReadKey ();
+            
+
+        }
+
+    }
+}
+
+
+
+
+
