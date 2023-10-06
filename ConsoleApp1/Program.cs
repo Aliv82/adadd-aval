1e8e4a8c0c89a3f781f55abce968859fcc830a2f
Ali.v_82 <alivah1382@gmail.com>
2023-10-06 22:09:50 +0330

    Ziba

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,47 +1,40 @@
-﻿using System.Collections.Generic;
+﻿
+// Ali vahedi
+// Adadd_Aval project
 
+Console.WriteLine("please insert your first number : ");
+int aval_Baze = Convert.ToInt32(Console.ReadLine());
 
-namespace adadd_aval
+Console.WriteLine("please insert your second number : ");
+int akhar_Baze = Convert.ToInt32(Console.ReadLine());
+
+for (int i = aval_Baze; i < akhar_Baze; i++)
 {
-    class adadd_aval
+    if (mohasebeh(i) && i>=2)
     {
-        static void Main()
+        Console.WriteLine(i);
+    }
+}
+
+static bool mohasebeh(int number)
+{
+    for (int j = 2; j < number; j++)
+    {
+        if (number % j == 0 && number != j)
         {
-            // Ali vahedi
-            // Adadd_Aval progect
-            Console.WriteLine("please insert your first number : ");
-            int aval_Baze = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("please insert your second number : ");
-            int akhar_Baze = Convert.ToInt32(Console.ReadLine());
-
-            
-
-            for (int i = aval_Baze; i < akhar_Baze; i++)
-            {
-                if (mohasebeh(i))
-                {
-                    Console.WriteLine(i);
-                }
-            }
-            static  bool mohasebeh(int number)
-            {
-                for (int j = 2; j < number; j++)
-                {
-                    if (number % j == 0 && number != j)
-                    {
-                        return false;
-                    }
-                   
-                }
-                return true;
-            }
-            Console.ReadKey ();
-            
-
+            return false;
         }
 
     }
+    return true;
 }
+Console.ReadKey();
+
+
+
+
+
+
 
 
 
